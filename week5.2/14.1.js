736ba9bf8187d1018c1f67a1ac00389bd65cef22
Arman-Kabir <armankabir8@gmail.com>
2023-01-12 11:18:21 +0600

    14.1 done

---
+++ week5.2/14.1.js
@@ -0,0 +1,12 @@
+// SORTING means -- Organizing data.
+// Elementary sorting. Time complexity- O(n^2). Space complexity - O(1).
+// Bubble sort(Gradually sort largest item to the right)--O(n^2)--in case almost sorted , works very fast
+// Selection sort(Gradually sort smallest item to the left/beginning)--O(n^2)
+// Insertion sort(Building sorted Largest left half gradually)--O(n^2)--If sorted data- then new data insertion is easy.
+
+
+
+// More efficient sorting algorithm
+// Merge Sort
+// Quick Sort
+// Radix Sort
