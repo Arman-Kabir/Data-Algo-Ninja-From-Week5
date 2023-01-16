6d885d39579fbcd0b4c5ca1caa53a279139c6036
Arman-Kabir <armankabir8@gmail.com>
2023-01-16 12:37:51 +0600

    class explored a little

--- week6.1/15.1.js
+++ week6.1/15.1.js
@@ -1,1 +1,21 @@
-// Linked List
+// Linked List - List where multiple elements will be linked
+
+class Person {
+    constructor(leg, arm) {
+        this.leg = leg;
+        this.arm = arm;
+    }
+    // properties
+    // leg = 2;
+    // arm = 2;
+
+    // method
+    walk() {
+        console.log('is walking......');
+    }
+}
+
+const samim = new Person(2, 2);
+const specialPerson = new Person(3, 2);
+// console.log(samim.walk());
+console.log(specialPerson);
