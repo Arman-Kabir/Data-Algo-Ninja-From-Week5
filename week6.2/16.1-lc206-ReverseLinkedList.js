ae47340d9166ef0ef2d0482f5f7279d713c224d9
Arman-Kabir <armankabir8@gmail.com>
2023-01-21 13:36:56 +0600

    reverse ll done

--- week6.2/16.1-lc206-ReverseLinkedList.js
+++ week6.2/16.1-lc206-ReverseLinkedList.js
@@ -1,1 +1,14 @@
 // 
+
+var reverseList = function (head) {
+    let prev = null
+    let curr = head
+    let forw = null
+    while(curr !=null){
+        forw = curr.next
+        curr.next = prev
+        prev = curr
+        curr = forw
+    }
+    return prev
+};
