5f4e46133a5464ab3692b5302d85346cf121101a
Arman-Kabir <armankabir8@gmail.com>
2023-02-11 11:01:45 +0600

    bst intro

--- week8.1/19.1.js
+++ week8.1/19.1.js
@@ -4,3 +4,11 @@
 // Heap
 // Priority Queue
 // HashTable
+
+
+/**Binary Tree-BT
+ * Every node will connect to 0 to 2 nodes except leaf nodes.
+ * A BT is full if every node has 0 or 2 children.
+ * A Bt ic complete BT if all levels are completely filled except possibly the last level
+ *  and the last level   
+ */
